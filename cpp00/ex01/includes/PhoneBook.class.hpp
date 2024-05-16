a90f6ab776776e4ed2f1f25db714ea563f897602
yusengok <yusengok@student.42.fr>
2024-05-16 15:42:35 +0200

    Add ex01(on going), update Makefile

---
+++ cpp00/ex01/includes/PhoneBook.class.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.class.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 13:07:59 by yusengok          #+#    #+#             */
+/*   Updated: 2024/05/16 15:40:40 by yusengok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_CLASS_HPP
+# define PHONEBOOK_CLASS_HPP
+
+#include <iostream>
+#include <string>
+
+class PhoneBook {
+
+	public:
+
+		PhoneBook( void );
+		~PhoneBook( void );
+
+	private:
+};
+
+# define RESET	"\033[0m"
+# define GREEN	"\033[1;32m"
+# define RED	"\033[1;31m"
+
+#endif
