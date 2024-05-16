59b6edb0f99b596ff876cd66796d3e02f08d0691
yusengok <ysengoku.tourisme@gmail.com>
2024-05-16 11:35:57 +0200

    Update ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 11:25:17 by yusengok          #+#    #+#             */
+/*   Updated: 2024/05/16 11:32:06 by yusengok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+int main(int argc, char **argv)
+{
+	if (argc <= 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::putwchar(toupper(argv[i][j]));
+			if (i == argc - 1)
+				std::putwchar('\n');
+			else
+				std::putwchar(' ');
+		}
+	}
+	return 0;
+}
