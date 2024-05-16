e5e79465e0913f6b06a6ca75d7025ded870306d8
yusengok <yusengok@student.42.fr>
2024-05-16 11:49:38 +0200

    Replace putwchar by cout

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 11:25:17 by yusengok          #+#    #+#             */
-/*   Updated: 2024/05/16 11:32:06 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/05/16 11:42:31 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 
 int main(int argc, char **argv)
 {
-	if (argc <= 1)
+	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
 		for (int i = 1; i < argc; i++)
 		{
 			for (int j = 0; argv[i][j]; j++)
-				std::putwchar(toupper(argv[i][j]));
+				std::cout << (char)(toupper(argv[i][j]));
 			if (i == argc - 1)
-				std::putwchar('\n');
+				std::cout << (char)('\n');
 			else
-				std::putwchar(' ');
+				std::cout << (char)(' ');
 		}
 	}
 	return 0;
