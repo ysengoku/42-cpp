80f2204256f20fd2be1e00e8cdac2058ef279b8d
ysengoku <ysengoku.tourisme@gmail.com>
2024-05-20 22:33:59 +0200

    Add display contact functions, update gitignore

--- cpp00/ex01/srcs/PhoneBook.cpp
+++ cpp00/ex01/srcs/PhoneBook.cpp
@@ -14,21 +14,15 @@
 
 /*===== Constructor & Destructor =============================================*/
 
-PhoneBook::PhoneBook( void )
+PhoneBook::PhoneBook( void ) : _count(0)
 {	
-	std::cout << CYAN << "================================" << RESET << std::endl;
-	std::cout << BGWHITE << BLACK  << "   Welcome to Mini PhoneBook    " << RESET << std::endl;
-	std::cout << CYAN << "================================" << RESET << std::endl;
-	std::cout << "Save a new contact -> " << GREEN << "ADD" << RESET << std::endl;
-	std::cout << "Display contact detail -> " << GREEN << "SEARCH" << RESET << std::endl;
-	std::cout << "Quit ->" << GREEN << "EXIT" << RESET << std::endl;
-	this->_count = 0;
+	PhoneBook::_printMenu();
 	return;
 }
 
 PhoneBook::~PhoneBook( void )
 {	
-	std::cout << "Thank you for using Mini Phone Book. See you soon 👋" << std::endl;
+	std::cout << "Thank you for using Mini Phone Book. See you soon 👋🏻" << std::endl;
 	return;
 }
 
@@ -38,8 +32,9 @@ void	PhoneBook::addContact( void )
 {
 	int i = 0;
 
-	if (this->_count == 7)
-		this->_replaceContact(&i);
+	std::cout << std::endl << BLUE << "▶︎▶︎▶︎ Adding new contact" << RESET << std::endl;
+	if (this->_count == 8)
+		i = this->_replaceContact();
 	else
 		i = this->_count;
 	this->_contacts[i].setFirstName();
@@ -47,37 +42,95 @@ void	PhoneBook::addContact( void )
 	this->_contacts[i].setNickname();
 	this->_contacts[i].setPhone();
 	this->_contacts[i].setDarkestSecret();
-	this->_count++;
+	if (this->_count < 8)
+		this->_count++;
 }
 
 void	PhoneBook::displayContact( void )
 {
-	this->_displayContactTable();
+	if (this->_count == 0)
+		std::cout << "You have no contacts yet." << std::endl << "To add a contact, type " << GREEN << "ADD" << RESET << std::endl << std::endl;
+	else
+	{
+		std::cout << std::endl << BLUE << "▶︎▶︎▶︎ Displaying your contact list" << RESET << std::endl;
+		this->_displayContactTable();
+		this->_displayContactDetail();
+	}
 }
 
 /*===== Private functions ====================================================*/
 
-void	PhoneBook::_replaceContact( int *i )
+void	PhoneBook::_printMenu( void ) const
 {
-	while (*i++ < 6)
-		this->_contacts[*i - 1] = this->_contacts[*i];
+	std::cout << "================================" << std::endl;
+	std::cout << BGWHITE << BLACK  << "   Welcome to Mini PhoneBook    " << RESET << std::endl;
+	std::cout << "================================" << std::endl;
+	std::cout << "Save a new contact -> " << GREEN << "ADD" << RESET << std::endl;
+	std::cout << "Display contact detail -> " << GREEN << "SEARCH" << RESET << std::endl;
+	std::cout << "Quit ->" << GREEN << "EXIT" << RESET << std::endl << std::endl;
+}
+
+int	PhoneBook::_replaceContact( void )
+{
+	for (int i = 0; i < 7; i++)
+		this->_contacts[i] = this->_contacts[i + 1];
+	return (7);
 }
 
 void	PhoneBook::_displayContactTable( void )
 {
 	std::cout << std::endl;
-	//std::cout << "----------------------------------------------" << std::endl;
-	std::cout << BGWHITE << BLACK <<" Index | First name | Last name  |  Nickname  " << RESET << std::endl;
-	//std::cout << "----------------------------------------------" << std::endl;
+	std::cout << BGWHITE << BLACK <<"     Index|First name| Last name|  Nickname" << RESET << std::endl;
 	for (int i = 0; i < this->_count; i++)
 	{
-		std::cout << "   " << i << "   |";
-		if (this->_contacts[i].getFirstName().length() > 10)
-			std::cout << " " << this->_contacts[i].getFirstName() << " |";
+		std::cout << "         " << i << "|";
+		this->_printTableElement(this->_contacts[i].getFirstName());
+		std::cout << "|";
+		this->_printTableElement(this->_contacts[i].getLastName());
+		std::cout << "|";
+		this->_printTableElement(this->_contacts[i].getNickname());
+		std::cout << std::endl << "----------+----------+----------+----------" <<std::endl;
 	}
 }
 
-//void	PhoneBook::_displayContactDetail( void )
-//{
-//
-//}
+void	PhoneBook::_printTableElement( std::string element )
+{
+	std::string	str;
+
+	if (element.length() > 9)
+		str = element.substr(0, 9) + ".";
+	else
+		str = element;
+	std::cout << std::setw(10) << str;
+}
+
+void	PhoneBook::_displayContactDetail( void )
+{
+	std::string	input;
+	int			i = -1;
+
+	std::cout << std::endl;
+	while (true)
+	{
+		std::cout << "Choose index >> ";
+		std::cin >> input;
+		if (input.empty() || input.length() > 1 || input[0] < '0' || input[0] > '7')
+			std::cout << YELLOW << "Invalid index. Please retry." << RESET << std::endl;
+		else
+		{
+			i =  input[0] - '0';
+			if (i > this->_count - 1)
+				std::cout << YELLOW << "Index " << i << " is empty. Please retry." << RESET << std::endl;
+			else
+				break ;
+		}
+	}
+	std::cout << std::endl;
+	std::cout << BGWHITE << BLACK " Index " << input[0] << " " << RESET << std::endl;
+	std::cout << "First name     : " << this->_contacts[i].getFirstName() << std::endl;
+	std::cout << "Last name      : " << this->_contacts[i].getLastName() << std::endl;
+	std::cout << "Nickname       : " << this->_contacts[i].getNickname() << std::endl;
+	std::cout << "Phone number   : " << this->_contacts[i].getPhone() << std::endl;
+	std::cout << "Darkest secret : " << this->_contacts[i].getSecret() << std::endl;
+	std::cout << std::endl;
+}
--- cpp00/ex01/srcs/main.cpp
+++ cpp00/ex01/srcs/main.cpp
@@ -18,17 +18,22 @@ int	main(void)
 	PhoneBook	phonebook;
 	std::string	input;
 
-	while (std::string(input) != "EXIT")
+	while (true)
 	{
 		std::cout << CYAN << "Enter command " << "▶︎▶︎▶︎ " << RESET;
+//		std::getline(std::cin, input);
 		std::cin >> input;
-	
-		if (std::string(input) == "ADD")
+		if (input.empty())
+			continue;
+		if (input == "ADD")
 			phonebook.addContact();
-		else if (std::string(input) == "SEARCH")
+		else if (input == "SEARCH")
 			phonebook.displayContact();
-		else if (std::string(input) == "EXIT")
+		else if (input == "EXIT")
+		{
 			std::cout << RED << "Deleting phonebook data..." << RESET << std::endl;
+			break ;
+		}
 		else
 			std::cout << RED << "[Error] " << RESET << "Invalid command" << std::endl;
 	}
