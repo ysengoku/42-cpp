318a1da5ad8bee9cf93018e69f530d835fc4d8a8
ysengoku <ysengoku.tourisme@gmail.com>
2024-06-27 22:40:07 +0200

    Add random complaints

--- cpp01/ex05/srcs/Harl.cpp
+++ cpp01/ex05/srcs/Harl.cpp
@@ -33,20 +33,24 @@ void	Harl::complain( std::string level )
 
 void	Harl::debug( void )
 {
-	std::cout << "\033[94m[DEBUG]\033[0m I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << "[DEBUG]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl << std::endl;
 }
 
 void	Harl::info( void )
 {
-	std::cout << "\033[92m[INFO]\033[0m I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "[INFO]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl << std::endl;
 }
 
 void	Harl::warning( void )
 {
-	std::cout << "\033[93m[WARNING]\033[0m I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "[WARNING]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl << std::endl;
 }
 
 void	Harl::error( void )
 {
-	std::cout << "\033[91m[ERROR]\033[0m This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[ERROR]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl << std::endl;
 }
--- cpp01/ex05/srcs/main.cpp
+++ cpp01/ex05/srcs/main.cpp
@@ -11,14 +11,31 @@
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+#include <cstdlib>
+#include <ctime>
+
+void	randomComplaints(Harl* harl, int numberOfComplaints)
+{	
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int			levelsCount = 4;
+	int			randomIndex;
+
+	std::srand(std::time(0));
+	for (int i = 0; i < numberOfComplaints; i++)
+	{
+		randomIndex = std::rand() % levelsCount;
+		(*harl).complain(levels[randomIndex]);
+	}
+}
 
 int		main( void )
 {
-	Harl	harl;
+	Harl		harl;
 
 	harl.complain("DEBUG");
 	harl.complain("INFO");
 	harl.complain("WARNING");
 	harl.complain("ERROR");
+	randomComplaints(&harl, 6);
 	return (0);
 }
