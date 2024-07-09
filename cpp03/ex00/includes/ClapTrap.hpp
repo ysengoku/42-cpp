383654a56120de1c53005c098a2cafe001a0ea08
yusengok <ysengoku.tourisme@gmail.com>
2024-07-09 17:40:24 +0200

    Add a class (on working)

--- cpp03/ex00/includes/ClapTrap.hpp
+++ cpp03/ex00/includes/ClapTrap.hpp
@@ -6,13 +6,25 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 17:34:06 by yusengok          #+#    #+#             */
-/*   Updated: 2024/07/09 17:35:30 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/07/09 17:39:56 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
 # define CLAPTRAP_HPP
 
+# include <string>
 
+class ClapTrap
+{
+	public:
+		/* Constructor & destructor */
+		ClapTrap( std::string Name );
+		~ClapTrap( void );
+
+		
+	private:
+		std::string	_name;
+};
 
 #endif
