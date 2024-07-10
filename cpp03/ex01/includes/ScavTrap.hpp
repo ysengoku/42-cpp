925dc6b8fde869d29b2779a92f75e741b241f1fd
yusengok <ysengoku.tourisme@gmail.com>
2024-07-10 17:19:17 +0200

    Update cpp03 ex00, add ex01

---
+++ cpp03/ex01/includes/ScavTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 16:49:33 by yusengok          #+#    #+#             */
+/*   Updated: 2024/07/10 17:18:28 by yusengok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp" 
+
+class ScavTrap : public ClapTrap
+{
+	public:
+		ScavTrap( std::string Name );
+		ScavTrap( const ScavTrap& src);
+		ScavTrap& operator=( const ScavTrap& rhs );
+		~ScavTrap();
+
+		void	guardGate( void );
+
+	private:
+};
+
+#endif
+
+/*
+public: accessible from outside the class
+private: not accessible from outside the class
+protected: not accessible from outside the class but accessible in inherited classes.
+*/
