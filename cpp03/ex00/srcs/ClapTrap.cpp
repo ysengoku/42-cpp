aea0a97c5088d3b3209fc29147cdeb0b4f1ce43d
yusengok <yusengok@student.42.fr>
2024-07-10 08:24:55 +0200

    cpp03 ex00 on working

--- cpp03/ex00/srcs/ClapTrap.cpp
+++ cpp03/ex00/srcs/ClapTrap.cpp
@@ -6,7 +6,66 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 17:34:42 by yusengok          #+#    #+#             */
-/*   Updated: 2024/07/09 17:34:43 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/07/10 08:12:18 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+#define YELLOW "\033[33m"
+#define RESET "\033[0m"
+
+/*============================================================================*/
+/*       Constructor & destructor                                             */
+/*============================================================================*/
+
+ClapTrap::ClapTrap( std::string Name )
+	: _name(Name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << YELLOW << "ClapTrap: Default constructor called" << RESET << std::endl;
+}
+
+ClapTrap::ClapTrap( const ClapTrap& src )
+{
+	std::cout << YELLOW << "ClapTrap: Copy constructor called" << RESET << std::endl;
+	if (this != &src)
+		*this = src;
+}
+
+ClapTrap::~ClapTrap( void )
+{
+	std::cout << YELLOW << "ClapTrap: Destructor called" << RESET << std::endl;
+}
+
+/*============================================================================*/
+/*       Copy assignment operator                                             */
+/*============================================================================*/
+
+ClapTrap	&ClapTrap::operator=( const ClapTrap& rhs )
+{
+	if (this != &rhs)
+	{
+		this->_name = rhs._name;
+		this->_hitPoints = rhs._hitPoints;
+		this->_energyPoints = rhs._energyPoints;
+		this->_attackDamage = rhs._attackDamage;
+	}
+	return (*this);
+}
+		
+/*============================================================================*/
+/*       Public member functions                                              */
+/*============================================================================*/
+
+void	attack( const std::string& target )
+{
+	
+}
+
+void	takeDamage( unsigned int amount )
+{
+	
+}
+
+void	beRepaired( unsigned int amount )
+{}
