e1ea393d42b92f85016d3ef3080063f29f40e44c
yusengok <ysengoku.tourisme@gmail.com>
2024-07-10 13:29:38 +0200

    cpp03 ex00 On working

--- cpp03/ex00/srcs/ClapTrap.cpp
+++ cpp03/ex00/srcs/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 17:34:42 by yusengok          #+#    #+#             */
-/*   Updated: 2024/07/10 08:12:18 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/07/10 13:28:51 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,27 @@
 ClapTrap::ClapTrap( std::string Name )
 	: _name(Name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
 {
-	std::cout << YELLOW << "ClapTrap: Default constructor called" << RESET << std::endl;
+	std::cout << YELLOW << "ClapTrap " << this->_name << " appeared." << std::endl;
+	std::cout << "== His initial status ==" << std::endl \
+	<< "Hit points: " << this->_hitPoints << std::endl \
+	<< "Energy points: " << this->_energyPoints << std::endl \
+	<< "Attack damage: " << this->_attackDamage << RESET << std::endl << std::endl;
 }
 
 ClapTrap::ClapTrap( const ClapTrap& src )
 {
-	std::cout << YELLOW << "ClapTrap: Copy constructor called" << RESET << std::endl;
+	std::cout << YELLOW << "The copy of ClapTrap " << this->_name << " appeared." << RESET << std::endl;
 	if (this != &src)
 		*this = src;
 }
 
 ClapTrap::~ClapTrap( void )
 {
-	std::cout << YELLOW << "ClapTrap: Destructor called" << RESET << std::endl;
+	std::cout << std::endl << YELLOW << "ClapTrap " << this->_name << " disappeared." << std::endl;
+	std::cout << "== His Final status ==" << std::endl \
+	<< "Hit points: " << this->_hitPoints << std::endl \
+	<< "Energy points: " << this->_energyPoints << std::endl \
+	<< "Attack damage: " << this->_attackDamage << RESET << std::endl;
 }
 
 /*============================================================================*/
@@ -57,15 +65,33 @@ ClapTrap	&ClapTrap::operator=( const ClapTrap& rhs )
 /*       Public member functions                                              */
 /*============================================================================*/
 
-void	attack( const std::string& target )
+void	ClapTrap::attack( const std::string& target )
 {
-	
+	if (this->_energyPoints == 0)
+		return ;
+	// target loses this->_attackDamage hit points
+	--this->_energyPoints;
+	std::cout << "ClapTrap " << this->_name << " attacks " << target << \
+	", causing " << this->_attackDamage << " points of damage!" << std::endl;
+	std::cout << "== Current Energy points of " << this->_name << " is " \
+	<< this->_energyPoints << " points. ==" << std::endl;
 }
 
-void	takeDamage( unsigned int amount )
+void	ClapTrap::takeDamage( unsigned int amount )
 {
-	
+	this->_hitPoints -= amount;
+	std::cout << "ClapTrap " << this->_name << " has taken " \
+	<< amount << " points of damage!" << std::endl;
 }
 
-void	beRepaired( unsigned int amount )
-{}
+void	ClapTrap::beRepaired( unsigned int amount )
+{
+	if (this->_energyPoints == 0)
+		return ;
+	this->_hitPoints += amount;
+	--this->_energyPoints;
+	std::cout << "ClapTrap " << this->_name << " repaires itself, getting " \
+	<< amount << " points back." << std::endl;
+	std::cout << "== Current Energy points of " << this->_name << " is " \
+	<< this->_energyPoints << " points. ==" << std::endl;
+}
