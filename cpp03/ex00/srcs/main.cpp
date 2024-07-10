d57964c2f7aba257928583d84832ce248a5feb1b
yusengok <yusengok@student.42.fr>
2024-07-10 13:29:38 +0200

    cpp03 ex00 On working

--- cpp03/ex00/srcs/main.cpp
+++ cpp03/ex00/srcs/main.cpp
@@ -6,7 +6,24 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 17:34:52 by yusengok          #+#    #+#             */
-/*   Updated: 2024/07/09 17:34:53 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/07/10 13:23:34 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+void	test1( void )
+{
+	ClapTrap*	clapTrap = new ClapTrap( "Clappy" );
+	
+	clapTrap->attack( "Trappy" );
+	clapTrap->takeDamage(3);
+	clapTrap->beRepaired(2);
+	delete clapTrap;
+}
+
+int	main()
+{
+	test1();
+	return (0);
+}
