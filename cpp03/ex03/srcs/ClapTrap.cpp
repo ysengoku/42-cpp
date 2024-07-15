5d38f73202996df63d8e5053c2323f32770b60fc
yusengok <yusengok@student.42.fr>
2024-07-15 15:19:39 +0200

    Update constructors (Doesn't work, need to fix)

--- cpp03/ex03/srcs/ClapTrap.cpp
+++ cpp03/ex03/srcs/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 17:34:42 by yusengok          #+#    #+#             */
-/*   Updated: 2024/07/15 08:30:40 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/07/15 15:07:08 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,12 @@ ClapTrap::ClapTrap( void )
 
 ClapTrap::ClapTrap( std::string Name )
 	: _name(Name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << YELLOW << "ClapTrap: Constructor called." << RESET << std::endl;
+}
+
+ClapTrap::ClapTrap( std::string Name, unsigned int hitPoints, unsigned int energyPoints, unsigned int attackDamage)
+	: _name(Name), _hitPoints(hitPoints), _energyPoints(energyPoints), _attackDamage(attackDamage)
 {
 	std::cout << YELLOW << "ClapTrap: Constructor called." << std::endl;
 }
@@ -86,7 +92,7 @@ void	ClapTrap::takeDamage( unsigned int amount )
 		this->_hitPoints -= amount;
 	else
 		this->_hitPoints = 0;
-	std::cout << this->_name << " has taken " << amount << " points of damage!" << std::endl;
+	std::cout << "🤕 " << this->_name << " has taken " << amount << " points of damage!" << std::endl;
 }
 
 void	ClapTrap::beRepaired( unsigned int amount )
@@ -95,7 +101,7 @@ void	ClapTrap::beRepaired( unsigned int amount )
 	{
 		this->_hitPoints += amount;
 		--this->_energyPoints;
-		std::cout << this->_name << " repaires itself, getting " \
+		std::cout << "🧘 " << this->_name << " repaires itself, getting " \
 		<< amount << " points back." << std::endl;
 		return ;
 	}
--- cpp03/ex03/srcs/main.cpp
+++ cpp03/ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 17:34:52 by yusengok          #+#    #+#             */
-/*   Updated: 2024/07/15 11:22:35 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/07/15 15:15:52 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,6 @@
 void	constructorCheck( void )
 {
 	std::cout << CYAN << "====== CONSTRUCTOR CHECK ======" << RESET << std::endl;	
-	{
-		std::cout << CYAN << "++ Default constructor ++ " << RESET << std::endl;
-		DiamondTrap	a();
-	}
 	{
 		std::cout << CYAN << "++ Constructor ++ " << RESET << std::endl;
 		DiamondTrap	a("DIAMOPPY");
@@ -50,49 +46,62 @@ void	simpleTest( void )
 	diamondTrap.printStatus();
 }
 
-// void	randomTest( void )
-// {
-// 	std::cout << std::endl << CYAN << "====== RANDOM TEST ======" << RESET << std::endl;
-// 	FragTrap	fragTrap("FRAPPY");
-// 	int			randomNum;
+void	randomTest( void )
+{
+	std::cout << std::endl << CYAN << "====== RANDOM TEST ======" << RESET << std::endl;
+	DiamondTrap	diamondTrap("DIAMOPPY");
+	int			randomNum;
 	
-// 	std::srand(std::time(0));
-// 	while (fragTrap.getEnergyPoints() > 0 && fragTrap.getHitPoints() > 0)
-// 	{
-// 		randomNum = std::rand() % 3;
-// 		switch (randomNum)
-// 		{
-// 			case 0:
-// 				fragTrap.highFivesGuys();
-// 				break;
-// 			case 1:
-// 				fragTrap.takeDamage(15);
-// 				break;
-// 			case 2:
-// 				fragTrap.beRepaired(10);
-// 				break;
-// 			default:
-// 				break;
-// 		}
-// 		fragTrap.printStatus();
-// 	}
-// 	fragTrap.beRepaired(10);
-// }
+	std::srand(std::time(0));
+	while (diamondTrap.getEnergyPoints() > 0 && diamondTrap.getHitPoints() > 0)
+	{
+		randomNum = std::rand() % 5;
+		switch (randomNum)
+		{
+			case 0:
+				diamondTrap.attack("TRAPPY");
+				break;
+			case 1:
+				diamondTrap.highFivesGuys();
+				break;
+			case 2:
+				diamondTrap.takeDamage(15);
+				break;
+			case 3:
+				diamondTrap.beRepaired(10);
+				break;
+			case 4:
+				diamondTrap.whoAmI();
+			default:
+				break;
+		}
+		diamondTrap.printStatus();
+	}
+	diamondTrap.beRepaired(10);
+}
 
-// void	energyPointsTest( void )
-// {
-// 	std::cout << std::endl << CYAN << "====== ENERGY POINTS TEST ======" << RESET << std::endl;
-// 	FragTrap	fragTrap("FRAPPY");	
+void	energyPointsTest( void )
+{
+	std::cout << std::endl << CYAN << "====== ENERGY POINTS TEST ======" << RESET << std::endl;
+	DiamondTrap	diamondTrap("DIAMOPPY");	
 
-// 	for (int i = 0; i <= 100; i++)
-// 		fragTrap.beRepaired(1);
-// }
+	for (int i = 0; i <= 100; i++)
+	{
+		if (i % 2 == 0)
+			diamondTrap.attack("TRAPPY");
+		else
+			diamondTrap.beRepaired(1);
+		if (diamondTrap.getEnergyPoints() == 0)
+			break ;
+	}
+	diamondTrap.attack("TRAPPY");
+}
 
 int	main()
 {
 	constructorCheck();
 	simpleTest();
-	// randomTest();
-	// energyPointsTest();
+	randomTest();
+	energyPointsTest();
 	return (0);
 }
