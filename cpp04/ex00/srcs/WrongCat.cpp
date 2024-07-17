5c1d6aa964e31ed1da38999602482f32601aae36
yusengok <yusengok@student.42.fr>
2024-07-17 14:00:45 +0200

    Add some tests

--- cpp04/ex00/srcs/WrongCat.cpp
+++ cpp04/ex00/srcs/WrongCat.cpp
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 08:10:51 by yusengok          #+#    #+#             */
-/*   Updated: 2024/07/17 12:45:17 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/07/17 13:24:18 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,19 @@
 WrongCat::WrongCat( void )
 	: WrongAnimal("WrongCat")
 {
-	std::cout << BLACKI << "WrongCat: Default constructor called." << std::endl;
+	std::cout << BLACKI << "WrongCat: Default constructor called." << RESET << std::endl;
 }
 
 WrongCat::WrongCat( const WrongCat& src )
 	: WrongAnimal(src)
 {
-	std::cout << BLACKI << "WrongCat: Copy constructor called."	<< std::endl;
+	std::cout << BLACKI << "WrongCat: Copy constructor called." << RESET << std::endl;
 	*this = src;
 }
 
 WrongCat& WrongCat::operator=( const WrongCat& rhs )
 {
-	std::cout << BLACKI << "WrongCat: Copy assignment operator called."	<< std::endl;
+	std::cout << BLACKI << "WrongCat: Copy assignment operator called."	<< RESET << std::endl;
 	if (this != &rhs)
 		this->_type = rhs._type;
 	return (*this);
@@ -39,7 +39,7 @@ WrongCat& WrongCat::operator=( const WrongCat& rhs )
 
 WrongCat::~WrongCat( void )
 {
-	std::cout << BLACKI << "WrongCat: Destructor called."	<< std::endl;
+	std::cout << BLACKI << "WrongCat: Destructor called." << RESET << std::endl;
 }
 
 /*============================================================================*/
