009ceaa78f5e18c37e702eb73a8ed61b4e0e431e
yusengok <yusengok@student.42.fr>
2024-07-29 12:57:30 +0200

    Add some tests, Update form name error message

--- cpp05/ex03/srcs/main.cpp
+++ cpp05/ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 09:02:59 by yusengok          #+#    #+#             */
-/*   Updated: 2024/07/29 10:47:46 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/07/29 12:56:17 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,13 +64,16 @@ int	main( void )
 
 	formTest("Shrubbery Creation", "Garden", 100, intern);
 	formTest("Shrubbery creation Form", "Garden", 100, intern);
+	formTest("Shrubbery ccreation Form", "Garden", 100, intern);
 
 	formTest("Robotomy Request", "Cyborg", 42, intern);
 	formTest("ROBOTOMY REQUEST FORM", "Cyborg", 42, intern);
+	formTest("Robotomy Requestt", "Cyborg", 42, intern);
 
 	formTest("Presidential Pardon", "Marvin", 2, intern);
 	formTest("presidential pardon", "Marvin", 2, intern);
 	formTest("presidential pardon form", "Marvin", 2, intern);
+	formTest("pPresidential Pardon", "Marvin", 2, intern);
 
 	formTest("invalide form name", "Target", 100, intern);
 	formTest("", "Target", 100, intern);
