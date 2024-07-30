4c75b1a8170f83b8a357c79486174f92e7bc3529
ysengoku <ysengoku.tourisme@gmail.com>
2024-07-30 22:07:23 +0200

    Add class members declaration

--- cpp06/ex00/includes/ScalarConverter.hpp
+++ cpp06/ex00/includes/ScalarConverter.hpp
@@ -15,18 +15,57 @@
 
 # include <string>
 # include <iostream>
+# include <limits>
+# include <cmath>
 
 class	ScalarConverter
 {
 	public:
-		static void	convert( std::string const& litteral );
+		static void	convert( std::string const& literal );
+
+		enum	inputType
+		{
+			CHAR,
+			INT,
+			FLOAT,
+			DOUBLE,
+		};
+
+		struct Scalar
+		{
+			inputType	type;
+			char		charValue;
+			int			intValue;
+			float		floatValue;
+			double		doubleValue;
+		};
+		
+		class	InvalidInputException	: std::exception
+		{
+			public:
+				virtual const char*	what() const throw();
+		};
 
 	private:
 		ScalarConverter( void );
 		ScalarConverter( ScalarConverter const& src );
 		ScalarConverter& operator=( ScalarConverter const& rhs );
 		~ScalarConverter( void );
+
+		inputType	identifyType( std::string const& literal ) const;
+		bool		isChar( std::string const& literal ) const;
+		bool		isInt( std::string const& literal ) const;
+		bool		isFloat( std::string const& literal ) const;
+		bool		isDouble( std::string const& literal ) const;
+
+		void	convertValues( std::string const& literal, ScalarConverter::Scalar &scalar ) const;
+		char	convertToChar( std::string const& literal )  const;
+		int		convertToInt( std::string const& literal )  const;
+		float	convertToFloat( std::string const& literal )  const;
+		double	convertToDouble( std::string const& literal )  const;
 };
+
+std::ostream&	operator<<( std::ostream& os, ScalarConverter::Scalar const& scalar );
 
 # define RED "\033[31m"
 # define GREEN "\033[32m"
--- cpp06/ex00/srcs/main.cpp
+++ cpp06/ex00/srcs/main.cpp
@@ -10,3 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+
+int	main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "Usage: ./convert <value to convert>" << std::endl;
+		return (1);
+	}
+	try
+	{
+		ScalarConverter::convert(argv[1]);
+	}
+	catch(const ScalarConverter::InvalidInputException& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	return (0);
+}
