66ddd7288e0d9c188388643980cdf1c6a633b82d
yusengok <ysengoku.tourisme@gmail.com>
2024-08-02 09:28:38 +0200

    Update print message

--- cpp06/ex01/srcs/main.cpp
+++ cpp06/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 15:22:53 by yusengok          #+#    #+#             */
-/*   Updated: 2024/08/01 16:12:33 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/08/02 09:26:55 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@ int	main( void )
 	uintptr_t	serialized;
 	Data		*deserialized;
 	
-	std::cout << CYAN << "------ Original ------" << RESET << std::endl
+	std::cout << CYAN << "--- Original ------" << RESET << std::endl
 	<< "pointer: " << &data << std::endl
 	<< data << std::endl;
 
 	serialized = Serializer::serialize(&data);
 	deserialized = Serializer::deserialize(serialized);
 
-	std::cout << CYAN << "------ After deserializing ------" << RESET << std::endl
+	std::cout << CYAN << "--- After deserializing ------" << RESET << std::endl
 	<< "pointer: " << deserialized << std::endl
 	<< *deserialized << std::endl;
 	if (&data == deserialized)
