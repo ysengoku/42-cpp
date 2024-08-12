d7aa4e30a1bbc8b9e8d0bc0da8f03ebcc10dc26e
ysengoku <ysengoku.tourisme@gmail.com>
2024-08-12 20:54:46 +0200

    Add tests

--- cpp07/ex02/srcs/main.cpp
+++ cpp07/ex02/srcs/main.cpp
@@ -13,15 +13,95 @@
 #include "Array.hpp"
 
 #define CYAN "\033[36m"
+#define RED "\033[31m"
 #define RESET "\033[0m"
+#define MAX_VAL 750
+
+void testFromSubject(void)
+{
+	std::cout << CYAN << "====== Tests from subject ======" << RESET << std::endl;
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    std::cout << "--- Copy constructor & copy assignment operator ---" << std::endl;
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+		Array<int> tmp2;
+		tmp2 = numbers;
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << RED << "didn't save the same value!!" << RESET << std::endl;
+            return ;
+        }
+    }
+	std::cout << "--- Negative index value ---" << std::endl;
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << RED << e.what() << RESET << '\n';
+    }
+	std::cout << "--- Too big index value ---" << std::endl;
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << RED << e.what() << RESET << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+
+	std::cout << "--- Extra tests by me ---" << std::endl;
+	{
+		Array<int> tmp(numbers);
+		for (int i = 0; i < 5; i++)
+		{
+			int index = rand() % MAX_VAL;
+			tmp [index] = numbers[index] + 1;
+			std::cout << "numbers[" << index << "] " << numbers[index] << std::endl;
+			std::cout << "tmp[" << index << "] " << tmp[index] << std::endl;
+		}
+	}
+	{
+		Array<int> tmp; 
+		tmp = numbers;
+		for (int i = 0; i < 5; i++)
+		{
+			int index = rand() % MAX_VAL;
+			tmp [index] = numbers[index] + 1;
+			std::cout << "numbers[" << index << "] " << numbers[index] << std::endl;
+			std::cout << "tmp[" << index << "] " << tmp[index] << std::endl;
+		}
+	}
+    delete [] mirror;
+}
 
 void emptyArray(void) {
-	std::cout << CYAN << "====== Empty array ======" << RESET << std::endl;
+	std::cout << std::endl << CYAN << "====== Empty array ======" << RESET << std::endl;
 	Array<int> array;
 	// std::cout << "Array content: " << array[0] << std::endl;
 }
 
 int main(void) {
+	testFromSubject();
 	emptyArray();
 	return (0);
 }
