040c33b1b2767528ee41388e147e4866fc622aad
yusengok <ysengoku.tourisme@gmail.com>
2024-08-13 10:31:14 +0200

    Update test

--- cpp07/ex01/srcs/main.cpp
+++ cpp07/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 08:02:36 by yusengok          #+#    #+#             */
-/*   Updated: 2024/08/13 09:54:50 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/08/13 10:30:32 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,6 +87,27 @@ void testChar2(void) {
 	std::cout << std::endl << std::endl;
 }
 
+void stringToUpper(std::string& str) {
+	unsigned int len = str.length();
+	::iter(&str, len, ::toupperWrapper);
+}
+
+void testString(void) {
+	std::cout << CYAN << "===== Test 5 - string/toupper =====" << RESET << std::endl; 
+	std::string array[] = {"aaa", "bBb", "CCc!"};
+	size_t len = sizeof(array) / sizeof(std::string);
+	std::cout << "BEFORE:" << std::endl;
+	for (size_t i = 0; i < len; i++)
+		std::cout << array[i] << std::endl;
+		
+	::iter(&array, len, ::stringToUpper);
+	
+	std::cout << "AFTER:" << std::endl;
+	for (size_t i = 0; i < len; i++)
+		std::cout << array[i] << std::endl;
+	std::cout << std::endl;
+}
+
 void writeA(size_t const& n) {
 	for (size_t i = 0; i < n; i++)
 		std::cout << 'A';
@@ -94,29 +115,11 @@ void writeA(size_t const& n) {
 }
 
 void testSizeT(void) {
-	std::cout << CYAN << "===== Test 5 - size_t =====" << RESET << std::endl; 
+	std::cout << CYAN << "===== Test 6 - size_t =====" << RESET << std::endl; 
 	size_t array[] = {2, 15, 0, 5, 20};
 	size_t len = sizeof(array) / sizeof(size_t);
 	::iter(&array, len, ::writeA);
 	std::cout << std::endl;
-}
-
-void stringToUpper(std::string& str) {
-	for (int i = 0; str[i]; i++)
-		str[i] = std::toupper(str[i]);
-}
-
-void testString(void) {
-	std::cout << CYAN << "===== Test 6 - string/toupper =====" << RESET << std::endl; 
-	std::string array[] = {"aaa", "bBb", "CCc!"};
-	size_t len = sizeof(array) / sizeof(std::string);
-	std::cout << "BEFORE:" << std::endl;
-	for (size_t i = 0; i < len; i++)
-		std::cout << array[i] << std::endl;
-	::iter(&array, len, ::stringToUpper);
-	std::cout << "AFTER:" << std::endl;
-	for (size_t i = 0; i < len; i++)
-		std::cout << array[i] << std::endl;
 }
 
 int main(void) {
@@ -125,7 +128,7 @@ int main(void) {
 	testDouble();
 	testChar();
 	testChar2();
-	testSizeT();
 	testString();
+	testSizeT();
 	return 0;
 }
