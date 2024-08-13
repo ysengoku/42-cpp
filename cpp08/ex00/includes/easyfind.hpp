4e00ec51b63fef3fa6c1c7c1e50d20f6b2972649
yusengok <ysengoku.tourisme@gmail.com>
2024-08-13 15:06:42 +0200

    Update ex00 (on working)

--- cpp08/ex00/includes/easyfind.hpp
+++ cpp08/ex00/includes/easyfind.hpp
@@ -6,7 +6,19 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 14:26:44 by yusengok          #+#    #+#             */
-/*   Updated: 2024/08/13 14:26:45 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/08/13 14:51:05 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <cstdlib>
+
+template< template <typename> typename T >
+int easyfind(T<int> const& c, int const& n);
+
+# include "easyfind.tpp"
+
+#endif
