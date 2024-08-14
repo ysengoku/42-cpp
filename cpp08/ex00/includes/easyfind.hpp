4b3cda9a154349311d9a9fe62944288179e4552f
yusengok <ysengoku.tourisme@gmail.com>
2024-08-14 14:27:00 +0200

    Update ex00 (on working)

--- cpp08/ex00/includes/easyfind.hpp
+++ cpp08/ex00/includes/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 14:26:44 by yusengok          #+#    #+#             */
-/*   Updated: 2024/08/13 14:51:05 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/08/14 11:24:09 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,24 @@
 
 # include <iostream>
 # include <cstdlib>
+# include <algorithm>
 
 template< template <typename> typename T >
 int easyfind(T<int> const& c, int const& n);
 
+class NoOccurrenceException : std::exception {
+	virtual const char* what() const throw();
+}
+
 # include "easyfind.tpp"
+
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE  "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN "\033[36m"
+# define GREY "\e[0;90m"
+# define RESET "\033[0m"
 
 #endif
--- cpp08/ex00/includes/easyfind.tpp
+++ cpp08/ex00/includes/easyfind.tpp
@@ -6,13 +6,23 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 14:26:47 by yusengok          #+#    #+#             */
-/*   Updated: 2024/08/13 14:51:21 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/08/14 14:04:23 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
+# define RED "\033[31m"
+# define RESET "\033[0m"
+
 template< template <typename> typename T >
 int easyfind(T<int> const& c, int const& n) {
-	
+	T<int>::iterator it = std::find(c.begin(), c.end(), n);
+	if (it == c.end())
+		throw NoOccurrenceException();
+	return (*it);
+}
+
+const char* NoOccurrenceException::what() const throw() {
+	return (RED "No occurrence is found." RESET);
 }
