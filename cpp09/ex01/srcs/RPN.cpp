4918bcfa813f2f5fc9ce19e44e4f442a302142c3
yusengok <yusengok@student.42.fr>
2024-09-04 10:43:12 +0200

    Update ex01

--- cpp09/ex01/srcs/RPN.cpp
+++ cpp09/ex01/srcs/RPN.cpp
@@ -47,33 +47,108 @@ RPN::~RPN() {
 /*============================================================================*/
 
 void RPN::calculate(void) {
-	// std::istringstream iss(_input);
-	// std::string n1;
-	// std::string n2;
-	// int num1;
-	// int num2;
-	// char op;
-	
+	std::istringstream	iss(_input);
+	std::string			token;
+	char				*p_end;
 
-	
+	if (_input.empty())
+		throw EmptyInputException();
+	while (iss >> token) {
+		if (isOperator(token)) {
+			try {
+				performOperation(token[0]);
+			}
+			catch(const std::exception& e) {
+				std::cerr << e.what();
+				return;
+			}
+		}
+		else if (isValidNumber(token))
+			_stack.push(strtol(token.c_str(), &p_end, 10));
+		else
+			throw InvalidValueException();
+	}
+	if (_stack.size() == 1)
+		std::cout << _stack.top() << std::endl;
+	else
+		throw TooManyOperandsException();
 }
 
 /*============================================================================*/
 /*       Private member functions                                             */
 /*============================================================================*/
 
-bool RPN::isNumeric(void) {
-
+bool RPN::isOperator(std::string const& token) {
+	if (token == "+" || token == "-" || token == "/" || token == "*")
+		return (true);
+	return (false);
 }
 
-bool RPN::readInput(void) {
+bool RPN::isValidNumber(std::string& token) {
+	if (token.length() > 2)
+		return (false);
+	std::string::iterator it = token.begin();
+	std::string::iterator ite = token.end();
+	while (it != ite) {
+		if (!(isdigit(*it) || *it == '-'))
+			return (false);
+		++it;
+	}
+	if (atoi(token.c_str()) > 10 || atoi(token.c_str()) < -10)
+		return (false);
+	return (true);
+}
 
+void RPN::performOperation(char token) {
+	long res;
+	long operand1;
+	long operand2;
+
+	if (_stack.size() < 2)
+		throw TooFewOperandsException();
+	operand2 = _stack.top();
+	_stack.pop();
+	operand1 = _stack.top();
+	_stack.pop();
+	switch (token) {
+		case '+':
+			res = operand1 + operand2;
+			break ;
+		case '-':
+			res = operand1 - operand2;
+			break ;
+		case '/':
+			if (operand2 == 0)
+				throw DivisionByZeroException();
+			res = operand1 / operand2;
+			break ;
+		case '*':
+			res = operand1 * operand2;
+			break ;
+	}
+	_stack.push(res);
 }
 
 /*============================================================================*/
 /*       Exceptions                                                           */
 /*============================================================================*/
 
+const char* RPN::EmptyInputException::what() const throw() {
+	return (RED "Error: Input is empty.\n" RESET);
+}
+
 const char* RPN::InvalidValueException::what() const throw() {
-	return (RED "Error: Invalid value in the input\n" RESET);
+	return (RED "Error: Invalid value detected in the input expression.\n" RESET);
+}
+
+const char* RPN::TooManyOperandsException::what() const throw() {
+	return (RED "Error: Extra operand(s) detected.\n" RESET);
+}
+
+const char* RPN::TooFewOperandsException::what() const throw() {
+	return (RED "Error: Insufficient operands for operation.\n" RESET);
+}
+
+const char* RPN::DivisionByZeroException::what() const throw() {
+	return (RED "Error: Division by zero cannot be performed.\n" RESET);
 }
