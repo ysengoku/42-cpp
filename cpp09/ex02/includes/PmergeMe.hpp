a532d6e415902b4a13a53147109bf208234211c2
yusengok <ysengoku.tourisme@gmail.com>
2024-09-06 14:41:32 +0200

    On working

--- cpp09/ex02/includes/PmergeMe.hpp
+++ cpp09/ex02/includes/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 11:32:42 by yusengok          #+#    #+#             */
-/*   Updated: 2024/09/05 14:33:15 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/09/06 13:16:39 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <iostream>
 # include <string>
 # include <vector>
-# include <deque>
+# include <list>
 # include <cstdlib>
 # include <climits>
 # include <cerrno>
@@ -37,9 +37,14 @@ class PmergeMe {
 		PmergeMe(PmergeMe const& src);
 		PmergeMe& operator=(PmergeMe const& rhs);
 
-		void sort(void);
+		void pmergeme(void);
 		
 		class InvalidValueException : public std::exception {
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class DuplicateFoundException : public std::exception {
 			public:
 				virtual const char* what() const throw();
 		};
@@ -48,76 +53,110 @@ class PmergeMe {
 		char** _input;
 		size_t	_size;
 		std::vector<int> _containerV;
-		std::deque<int> _containerD;
-		double _timeV;
-		double _timeD;
+		std::list<int> _containerL;
 
 		PmergeMe(void);
 		
-		bool isValidValue(char*);
+		bool isValidValue(char const*);
 		void printTime(std::string const& containerType, double time);
+		void mergeInsertSort(std::vector<int>&);
+		void mergeInsertSort(std::list<int>&);
 
 		template<template <typename, typename> class Container>
-		double mergeInsertSort(Container< int, std::allocator<int> >& container) {
-			int straggler;
+		double sortSequence(Container< int, std::allocator<int> >& container) {
 			struct timeval begin, end;
 			gettimeofday(&begin, 0);
 			// clock_t start, end;
 			// start = clock();
-			Container< int, std::allocator<int> > tmp;
-			Container< int, std::allocator<int> > sortedSequence;
+
 			for (int i = 1; _input[i]; ++i) {
 				if (!isValidValue(_input[i]))
 					throw InvalidValueException();
-				tmp.push_back(atoi(_input[i]));
+				container.push_back(atoi(_input[i]));
 			}
-			// ----- Check duplicated elements
-			if (_size % 2 != 0) {
-				straggler = tmp.back();
-				tmp.pop_back();
-			}
-
-// Group the elements of X into [n/2] pairs of elements, arbitrarily, leaving one element unpaired if there is an odd number of elements.
-// Perform [n/2] comparisons, one per pair, to determine the larger of the two elements in each pair.
-
-// Recursively sort the [n/2] larger elements from each pair, creating a sorted sequence S of [n/2] of the input elements, in ascending order.
-// Insert at the start of S the element that was paired with the first and smallest element of S.
-// Insert the remaining [n/2] - 1 elements of X ∖ S into S, one at a time, with a specially chosen insertion ordering described below. Use binary search in subsequences of S (as described below) to determine the position at which each element should be inserted.
-
-			/* Sort each pair */
-			typename Container< int, std::allocator<int> >::iterator it = tmp.begin();
-			typename Container< int, std::allocator<int> >::iterator ite = tmp.end();
-			while (it != ite) {
-				if (*it > *(it + 1))
-					std::swap(*it, *(it + 1));
-				std::advance(it, 2);
-			}
-			// std::cout << tmp << std::endl;
-			
-			// Sort according to grater value of pair (recursive)
-			// Push the first pair to container
-			// Push larger ones of each pair to container
-			// Insert the remaining of tmp by using binary search
-
-			if (_size % 2 != 0)
-				tmp.push_back(straggler); /// ------------- temporary
-				// insert the straggler by using binary search
-			container = tmp; /// ------------- temporary
-
+			if (!checkDuplicate(container))
+				throw DuplicateFoundException();
+			mergeInsertSort(container);
 			gettimeofday(&end, 0);
-			long sec = end.tv_sec - begin.tv_sec;
-			long usec = end.tv_usec - begin.tv_usec;
-			return (sec * 1000000 + usec);
+			double sec = end.tv_sec - begin.tv_sec;
+			double usec = end.tv_usec - begin.tv_usec;
+			return (sec * 1000000.0 + usec);
 			// end = clock();
 			// double sec = (end - start) / CLOCKS_PER_SEC;
 			// return  (sec * 1000000.0);
-		}		
+		}
+
+		template<template <typename, typename> class Container>
+		bool checkDuplicate(Container< int, std::allocator<int> > const& container) {
+			typedef typename Container< int, std::allocator<int> >::const_iterator const_iterator;
+			const_iterator itBegin = container.begin();
+			const_iterator itEnd = container.end();
+			while (itBegin != itEnd) {
+				const_iterator it = itBegin;
+				while(++it != itEnd) {
+					if (*itBegin == *it)
+						return (false);
+				}
+				++itBegin;
+			}
+			return (true);
+		}
+		
+		// template<template <typename, typename> class Container>
+		// void mergeInsertSort(Container< int, std::allocator<int> >& container) {
+		// 	int straggler;
+		// 	// Container< int, std::allocator<int> > tmp = container;
+		// 	Container< int, std::allocator<int> > largerNumbers;
+		// 	Container< int, std::allocator<int> > smallerNumbers;
+		// 	Container< int, std::allocator<int> > sortedSequence;
+			
+		// /* Group the elements of X into [n/2] pairs of elements */
+						 
+		// /* leaving one element unpaired if there is an odd number of elements. */
+		// 	if (_size % 2 != 0) {
+		// 		straggler = container.back();
+		// 		container.pop_back();
+		// 	}
+
+		// /* Perform [n/2] comparisons, one per pair, to determine the larger of the two elements in each pair. */
+		// /* Recursively sort the [n/2] larger elements from each pair,
+		// creating a sorted sequence S of [n/2] of the input elements, in ascending order.*/
+		// 	typename Container< int, std::allocator<int> >::iterator it = container.begin();
+		// 	typename Container< int, std::allocator<int> >::iterator ite = container.end();
+		// 	while (it != ite) {
+		// 		if (*it > *(it + 1)) {
+		// 			largerNumbers.push_back(*it);
+		// 			smallerNumbers.push_back(*(it + 1));
+		// 		}	
+		// 		else {
+		// 			largerNumbers.push_back(*(it + 1));
+		// 			smallerNumbers.push_back(*it);
+		// 		}
+		// 		std::advance(it, 2);
+		// 	}
+		// 	// mergeInsertSort(largerNumbers);
+		// 	sortedSequence = largerNumbers;
+
+		// /* Insert at the start of S the element that was paired with the first and smallest element of S */
+
+		// /* Insert the remaining [n/2] - 1 elements of X ∖ S into S, one at a time,
+		// with a binary search in subsequences of S (as described below) 
+		// to determine the position at which each element should be inserted. */
+			
+		// 	if (_size % 2 != 0)
+		// 		sortedSequence.push_back(straggler); /// ------------- temporary
+		// 		// insert the straggler by using binary search
+		// 	container = sortedSequence; /// ------------- temporary
+		// }
 };
+
+std::ostream& operator<<(std::ostream& os, char**);
 
 template<template <typename, typename> class Container>
 std::ostream& operator<<(std::ostream& os, const Container< int, std::allocator<int> >& container) {
-	typename Container< int, std::allocator<int> >::const_iterator it = container.begin();
-	typename Container< int, std::allocator<int> >::const_iterator ite = container.end();
+	typedef typename Container< int, std::allocator<int> >::const_iterator const_iterator;
+	const_iterator it = container.begin();
+	const_iterator ite = container.end();
 	while (it != ite) {
 		os << ' ' << *it;
 		++it;
@@ -135,102 +174,3 @@ std::ostream& operator<<(std::ostream& os, const Container< int, std::allocator<
 # define RESET "\033[0m"
 
 #endif
-
-// template<template <typename, typename> class Container>
-// class PmergeMe {
-// 	private:
-// 		Container<int, std::allocator<int> > _container;
-// 		int	_elementCount;
-// 		double _time;
-		
-// 		PmergeMe(void);
-
-// 		bool isValidValue(char* value)  {
-// 			errno = 0;
-// 			char* p_end;
-// 			long num = strtol(value, &p_end, 10);
-			
-// 			if (*p_end != '\0' || errno == ERANGE || num > INT_MAX || num < INT_MIN)
-// 				return (false);
-// 			return (true);
-// 		}
-
-// 	public:
-// 		PmergeMe(char** input) : _elementCount(0), _time(0.0) {
-// 			for (int i = 1; input[i]; ++i) {
-// 				if (!isValidValue(input[i]))
-// 					throw InvalidValueException();
-// 				_container.push_back(atoi(input[i]));
-// 				++_elementCount;
-// 			}
-// 			std::cout << GREY << "Constructor called." << RESET << std::endl;
-// 		}
-		
-// 		~PmergeMe(void) {
-// 			std::cout << GREY << "Destructor called." << RESET << std::endl;
-// 		}
-		
-// 		PmergeMe(PmergeMe const& src);
-// 		PmergeMe& operator=(PmergeMe const& rhs);
-
-// 		void sort(void) {
-// 			printTime();
-// 		}
-
-// 		void printNumbers(void)  {
-// 			typename Container<int, std::allocator<int> >::iterator it = _container.begin();
-// 			typename Container<int, std::allocator<int> >::iterator ite = _container.end();
-// 			while (it != ite - 1) {
-// 				std::cout << *it << ' ';
-// 				++it;
-// 			}
-// 			std::cout << *it << std::endl;
-// 		}
-
-// 		void printTime(void) {
-// 			std::cout << std::fixed << std::setprecision(FLOATING_POINT_PRECISION);
-// 			std::cout << "Time to process a range of " \
-// 			<< _elementCount << " elements with std::vector : " \
-// 			<< _time  << " us " << std::endl;	
-// 		}
-
-// 		class InvalidValueException : public std::exception {
-// 			public:
-// 				virtual const char* what() const throw() {
-// 					return (RED "Error: Invalid value detected in the input.\n" RESET);
-// 				}
-// 		};
-// };
-
-// template<typename T>
-// void mergeInsertSort(T& numbers) {
-	
-// }
-
-// class PmergeMe {
-// 	private:
-// 		std::vector<int> _containerV;
-// 		std::list<int> _containerL;
-// 		int	_elementCount;
-// 		double _timeV;
-// 		double _timeL;
-
-// 		PmergeMe(void);
-
-// 		bool isValidValue(char*);
-
-// 	public:
-// 		PmergeMe(char** input);
-// 		~PmergeMe(void);
-// 		PmergeMe(PmergeMe const& src);
-// 		PmergeMe& operator=(PmergeMe const& rhs);
-
-// 		void sort(void);
-// 		void printNumbers(void);
-// 		void printTime(void);
-
-// 		class InvalidValueException : public std::exception {
-// 			public:
-// 				virtual const char* what() const throw();
-// 		};
-// };
--- cpp09/ex02/srcs/PmergeMe.cpp
+++ cpp09/ex02/srcs/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 11:32:48 by yusengok          #+#    #+#             */
-/*   Updated: 2024/09/05 14:33:21 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/09/06 14:41:06 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 /*============================================================================*/
 
 PmergeMe::PmergeMe(char** input, size_t size)
-: _input(input), _size(size), _timeV(0.0), _timeD(0.0) {
+: _input(input), _size(size) {
 	std::cout << GREY << "Constructor called." << RESET << std::endl;
 }
 
@@ -34,10 +34,10 @@ PmergeMe& PmergeMe::operator=(PmergeMe const& rhs) {
 	std::cout << GREY << "Copy assignment operator called." << RESET << std::endl;
 	if (this != &rhs) {
 		this->_containerV = rhs._containerV;
-		this->_containerD = rhs._containerD;
+		this->_containerL = rhs._containerL;
 		this->_size = rhs._size;
-		this->_timeV = rhs._timeV;
-		this->_timeD = rhs._timeD;
+		// this->_timeV = rhs._timeV;
+		// this->_timeL = rhs._timeL;
 	}
 	return (*this);
 }
@@ -48,21 +48,19 @@ PmergeMe::PmergeMe(void) {}
 /*       Public member functions                                              */
 /*============================================================================*/
 
-void PmergeMe::sort(void) {
+void PmergeMe::pmergeme(void) {
+	double _timeV;
+	double _timeL;
 	try {
-		_timeV = mergeInsertSort(_containerV);
-		_timeD = mergeInsertSort(_containerD);
-
-		std::cout << "Before:";
-		int i;
-		for (i = 1; _input[i]; ++i)
-			std::cout << ' ' << _input[i] << std::endl;
+		_timeV = sortSequence(_containerV);
+		_timeL = sortSequence(_containerL);
+		std::cout << "Before:" << _input + 1 << std::endl;
 		std::cout << "After: " << _containerV << std::endl;
 		printTime("std::vector", _timeV);
-		printTime("std::deque ", _timeD);
+		printTime("std::list  ", _timeL);
 	}
 	catch (std::exception &e) {
-		std::cerr << e.what();
+		std::cerr << e.what() << std::endl;
 	}
 }
 
@@ -70,7 +68,7 @@ void PmergeMe::sort(void) {
 /*       Private member functions                                             */
 /*============================================================================*/
 
-bool PmergeMe::isValidValue(char* value) {
+bool PmergeMe::isValidValue(char const* value) {
 	errno = 0;
 	char* p_end;
 	long num = strtol(value, &p_end, 10);
@@ -80,10 +78,81 @@ bool PmergeMe::isValidValue(char* value) {
 	return (true);
 }
 
-void PmergeMe::printTime(std::string const& containerType, double time) {
+void PmergeMe::mergeInsertSort(std::vector<int>& vec) {
+	int straggler = -1;
+	std::vector<int> largerNumbers;
+	std::vector<int> smallerNumbers;
+
+	if (vec.size() == 1)
+		return; 
+	/* leaving one element unpaired if there is an odd number of elements. */
+	if (_size % 2 != 0) {
+		straggler = vec.back();
+		vec.pop_back();
+	}
+
+	/* Group the elements of X into [n/2] pairs of elements */
+	/* Perform [n/2] comparisons, one per pair, to determine the larger of the two elements in each pair. */
+	/* Recursively sort the [n/2] larger elements from each pair,
+	creating a sorted sequence S of [n/2] of the input elements, in ascending order.*/
+	std::vector<int>::iterator it = vec.begin();
+	std::vector<int>::iterator ite = vec.end();
+	while (it != ite && it + 1 != ite) {
+		if (*it > *(it + 1)) {
+			largerNumbers.push_back(*it);
+			smallerNumbers.push_back(*(it + 1));
+		}	
+		else {
+			largerNumbers.push_back(*(it + 1));
+			smallerNumbers.push_back(*it);
+		}
+		std::advance(it, 2);
+	}
+	
+	mergeInsertSort(largerNumbers);
+
+	/* Insert at the start of S the element that was paired with the first and smallest element of S */
+	int smallestInLarger = *(std::min_element(largerNumbers.begin(), largerNumbers.end()));
+	std::vector<int>::iterator smallestInLargerPos = std::find(vec.begin(), vec.end(), smallestInLarger);
+	int position = std::distance(vec.begin(), smallestInLargerPos);
+	int paired = (position % 2 == 0) ? vec.at(position + 1) : vec.at(position - 1);
+	largerNumbers.insert(largerNumbers.begin(), paired);
+	smallerNumbers.erase(std::find(smallerNumbers.begin(), smallerNumbers.end(), paired));
+
+	/* Insert the remaining [n/2] - 1 elements of X ∖ S into S, one at a time,
+	with a binary search in subsequences of S (as described below) 
+	to determine the position at which each element should be inserted. */
+	/// ------------- temporary ----------------------
+	largerNumbers.insert(largerNumbers.begin(), smallerNumbers.begin(), smallerNumbers.end());
+		
+	if (straggler != -1) {
+		/// ------------- temporary ------------------
+		std::vector<int>::iterator stragglerPos = std::lower_bound(largerNumbers.begin(), largerNumbers.end(), straggler);
+		largerNumbers.insert(stragglerPos, straggler);
+	// insert the straggler by using binary search
+	}
+	vec.clear();
+	vec.insert(vec.end(), largerNumbers.begin(), largerNumbers.end());
+}
+
+void PmergeMe::mergeInsertSort(std::list<int>& container) {
+	(void) container;
+}
+
+void PmergeMe::printTime(std::string const& numbersType, double time) {
 	std::cout << std::fixed << std::setprecision(FLOATING_POINT_PRECISION);
 	std::cout << "Time to process a range of " << _size << " elements with " \
-	<< containerType << " : " << time  << " us " << std::endl;	
+	<< numbersType << " : " << time  << " us " << std::endl;	
+}
+
+/*============================================================================*/
+/*       Operator overload                                                    */
+/*============================================================================*/
+
+std::ostream& operator<<(std::ostream& os, char** argv) {
+	for (int i = 0; argv[i]; ++i)
+		std::cout << ' ' << argv[i];
+	return (os);
 }
 
 /*============================================================================*/
@@ -91,6 +160,9 @@ void PmergeMe::printTime(std::string const& containerType, double time) {
 /*============================================================================*/
 
 const char* PmergeMe::InvalidValueException::what() const throw() {
-	return (RED "Error: Invalid value detected in the input.\n" RESET);
+	return (RED "Error: Invalid value detected in the input." RESET);
 }
 
+const char* PmergeMe::DuplicateFoundException::what() const throw() {
+	return (RED "Error: Duplicate number found." RESET);	
+}
