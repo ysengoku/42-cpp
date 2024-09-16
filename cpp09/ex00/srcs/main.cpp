42d82727335e1299f6db847e6ba8b4388673eb19
yusengok <ysengoku.tourisme@gmail.com>
2024-09-16 12:04:47 +0200

    Update input file open error handling using exception

--- cpp09/ex00/srcs/main.cpp
+++ cpp09/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 11:32:19 by yusengok          #+#    #+#             */
-/*   Updated: 2024/09/04 08:21:52 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/09/16 11:42:33 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,13 @@ int main(int argc, char** argv) {
 		std::cerr << "Usage: ./btc <file name>" << std::endl;
 		return (1);
 	}
-	BitcoinExcange btc(argv[1]);
-	btc.exchange();
+	try {
+		BitcoinExcange btc(argv[1]);
+		btc.exchange();
+	}
+	catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
 
 	// BitcoinExcange copy1(btc);
 	// copy1.exchange();
